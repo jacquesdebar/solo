0973e183f153fc7901907658131acff577840e6f
jacquesdebar <jacques.debar@gmail.com>
2020-03-05 21:31:32 -0800

    add solstice page

---
+++ src/pages/Storage.js
@@ -0,0 +1,94 @@
+
+            <div className="row my-5">
+            <div className="col-12">
+                <a href="http://students.washington.edu/jdebar/kiosk.html">
+                    <h2 className="mb-2">Husky Kiosk</h2>
+                </a>
+                <h3 className="team mb-3">
+                    with{" "}
+                    <a className="teammate" href="https://www.linkedin.com/in/bnguyen12/">
+                        Benny Nguyen
+                    </a>{" "}
+                    and{" "}
+                    <a className="teammate" href="https://www.linkedin.com/in/nicola-kalderash-bba38256/">
+                        Nicola Kalderash
+                    </a>
+                    .
+                </h3>
+                {/*<h3 className="course mb-3">INFO 360: Design Methods</h3>*/}
+                <div className="row justify-content-center">
+                    <Img src="/compHK.png" alt="Husky Kiosk screencaps" style={{ width: "100%" }} />
+                </div>
+                <div className="mt-2">
+                    <p>
+                        Husky Kiosk focuses on empowering formerly-incarcerated students at the University of
+                        Washington. Formerly-incarcerated people are one of the most marginalized and
+                        underrepresented demographics, particularly in higher education. Because of this, we sought
+                        to become allies and find a way for information technology to ease the undue burden on
+                        system-impacted people. After consulting many stakeholders, including formerly-incarcerated
+                        students themselves, we laid out a model for a physical space to serve as a gathering place
+                        and safe space for the system-impacted community at UW. In addition to this, we built an
+                        website to host informational resources to help provide information and education about
+                        topics of housing, finance, the UW landscape, and other resources pertinent to our
+                        stakeholders. This project was initially created for our INFO 360 (Design Inquiry) class.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div className="row my-5">
+            <div className="col-12">
+                <h2 className="mb-2">Swordfish</h2>
+                <h3 className="team mb-3">
+                    with{" "}
+                    <a className="teammate" href="https://www.linkedin.com/in/robert-lin1/">
+                        Robi Lin
+                    </a>
+                    ,{" "}
+                    <a className="teammate" href="https://www.linkedin.com/in/oscar-avatare/">
+                        Oscar Avatare
+                    </a>
+                    , and{" "}
+                    <a className="teammate" href="https://www.linkedin.com/in/lucas-woo-84103966/">
+                        Lucas Woo
+                    </a>
+                    .
+                </h3>
+                <div className="row justify-content-center">
+                    <Img src="/compSF.png" alt="Swordfish screencaps" style={{ width: "100%" }} />
+                </div>
+                <div className="mt-2">
+                    <p>
+                        Swordfish is an econometrics app centered built around stock regression. Since the stock
+                        market is so pervasive in our lives with the news, our retirement portfolios, and personal
+                        employment considerations, we figured building a tool to give users a little more insight
+                        into it would be very worthwhile, would be a great way to demonstrate how front-end and
+                        back-end programming could be brought together at a basic level. With Swordfish, users are
+                        able to see the relationships between a stock of their choice (such as AAPL, AMZN, TSLA,
+                        etc.) and a wide array of economic variables. This web app was made for our INFO 360
+                        (Client-Side Web Development) class.
+                    </p>
+                </div>
+            </div>
+        </div>
+        {/*     People are generally excited about stocks due to their speculative nature
+      so we decided to start here. R is an incredible tool which (among other things)
+      we used to build this application and think people who are interested in
+      econometrics would think R is a great place to start!
+
+      Swordfish was built to be an econometrics app with two overarching goals.
+
+To get people interested in econometrics
+To teach people something about econometrics
+
+Since the stock market is so pervasive in our lives with the news, our retirement
+portfolios, and personal employment considerations, we figured building a tool to
+give users a little more insight into it would be very worthwhile, and be a great
+way to demonstrate how front-end and back-end programming could be brought together
+to build an amazing tool such as this.
+
+The tools we used to create Swordfish include bootstrapping it with the Create React
+App, HTML, CSS, Javascript, React, R, Plumber, Digital Ocean, and Docker. But our most
+important tool enabling us was our own personal necessity. We had been yearning for a
+tool like this one to improve our own knowledge of investing, and now we hope it can
+benefit anyone and everyone that comes across this repo. */}
