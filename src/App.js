bb4b0162e9d14a4b65271f4710168bf6aafe48a8
jacquesdebar <jacques.debar@gmail.com>
2020-03-09 14:00:19 -0700

    remove spotify components

--- src/App.js
+++ src/App.js
@@ -1,66 +1,65 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ReactDom from 'react-dom';
-import Popup from 'react-popup';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ReactDom from "react-dom";
+import Popup from "react-popup";
 
 import Home from "./pages/Home";
 import Academics from "./pages/Academics";
 import Freelance from "./pages/Freelance";
 import Personal from "./pages/Personal";
 import Solstice from "./pages/Solstice";
-import Spotify from "./pages/Spotify";
+// import Spotify from "./pages/Spotify";
 import "./css/App.css";
 
 import { Layout } from "./components";
 
 const HomePage = () => (
-    <Layout>
-        <Home />
-    </Layout>
+  <Layout>
+    <Home />
+  </Layout>
 );
 
 const AcademicsPage = () => (
-    <Layout>
-        <Academics />
-    </Layout>
+  <Layout>
+    <Academics />
+  </Layout>
 );
 
 const FreelancePage = () => (
-    <Layout>
-        <Freelance />
-    </Layout>
+  <Layout>
+    <Freelance />
+  </Layout>
 );
 
 const PersonalPage = () => (
   <Layout>
     <Personal />
   </Layout>
-)
+);
 
 const SolsticePage = () => (
   <Layout>
     <Solstice />
   </Layout>
-)
+);
 
-const SpotifyPage = () => (
-  <Spotify />
-)
-
+// const SpotifyPage = () => (
+//   <Spotify />
+// )
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/academics" component ={AcademicsPage} />
-        <Route exact path="/freelance" component ={FreelancePage} />
-        <Route exact path="/personal" component ={PersonalPage} />
-        <Route exact path="/solstice" component ={SolsticePage} />
-        <Route exact path="/spotify" component ={SpotifyPage} />
-      </Switch>
-    </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/academics" component={AcademicsPage} />
+          <Route exact path="/freelance" component={FreelancePage} />
+          <Route exact path="/personal" component={PersonalPage} />
+          <Route exact path="/solstice" component={SolsticePage} />
+          {/* <Route exact path="/spotify" component ={SpotifyPage} /> */}
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
