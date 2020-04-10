4eea7f09c16995924039ca58b6ad301e064920b1
jacquesdebar <jacques.debar@gmail.com>
2020-04-10 13:46:35 -0700

    revert analytics

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ReactDom from "react-dom";
 import Popup from "react-popup";
-import { createBrowserHistory } from "history";
-import ReactGA from "react-ga";
 
 import Home from "./pages/Home";
 import Academics from "./pages/Academics";
@@ -11,16 +9,12 @@ import Freelance from "./pages/Freelance";
 import Personal from "./pages/Personal";
 import Solstice from "./pages/Solstice";
 import NMF from "./pages/NMF";
+// import Spotify from "./pages/Spotify";
 import "./css/App.css";
 
+// import { Home, Academics, Freelance, Personal, Solstice, NMF } from "./pages";
+
 import { Layout, Layout2 } from "./components";
-
-
-const history = createBrowserHistory();
-history.listen((location) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
 
 const HomePage = () => (
   <Layout>
@@ -62,18 +56,22 @@ const NMFPage = () => (
 //   <Spotify />
 // )
 
-const App = () => (
-  <BrowserRouter history={history}>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/academics" component={AcademicsPage} />
-      <Route exact path="/freelance" component={FreelancePage} />
-      <Route exact path="/personal" component={PersonalPage} />
-      <Route exact path="/solstice" component={SolsticePage} />
-      <Route exact path="/nmf" component={NMFPage} />
-      {/* <Route exact path="/spotify" component ={SpotifyPage} /> */}
-    </Switch>
-  </BrowserRouter>
-);
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/academics" component={AcademicsPage} />
+          <Route exact path="/freelance" component={FreelancePage} />
+          <Route exact path="/personal" component={PersonalPage} />
+          <Route exact path="/solstice" component={SolsticePage} />
+          <Route exact path="/nmf" component={NMFPage} />
+          {/* <Route exact path="/spotify" component ={SpotifyPage} /> */}
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ReactGA from "react-ga";
-
-ReactGA.initialize("UA-163305268-1");
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
