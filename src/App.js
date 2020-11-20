b52b04233faa584e916d7ac7a634ea66a08cde99
jacquesdebar <jacques.debar@gmail.com>
2020-11-20 15:57:40 -0600

    add titles to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { TitleComponent } from './components/TitleComponent.js';
+import { Layout, Layout2 } from "./components";
 
 import Home from "./pages/Home";
 import Academics from "./pages/Academics";
@@ -12,10 +14,27 @@ import Hunch from "./pages/Hunch"
 // import Spotify from "./pages/Spotify";
 import "./css/App.css";
 
-// import { Home, Academics, Freelance, Personal, Solstice, NMF } from "./pages";
+// analytics
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 
-import { Layout, Layout2 } from "./components";
 
+// add title function
+const withTitle = ({ component: Component, title }) => {
+  return class Title extends React.Component {
+      render() {
+          return (
+              <React.Fragment>
+                  <TitleComponent title={title} />
+                  <Component {...this.props} />
+              </React.Fragment>
+          );
+      }
+  };
+};
+
+
+// pages
 const HomePage = () => (
   <Layout>
     <Home />
@@ -54,26 +73,45 @@ const NMFPage = () => (
 
 const GoPage = () => <Go />
 
-
 const HunchPage = () => <Hunch />
 
 // const SpotifyPage = () => (
 //   <Spotify />
 // )
 
+
+// add titles
+const HomePageTitled = withTitle({ component: HomePage, title: 'JACQUES DEBAR' });
+const AcademicsPageTitled = withTitle({ component: AcademicsPage, title: 'Academics' });
+const FreelancePageTitled = withTitle({ component: FreelancePage, title: 'Freelance' });
+const PersonalPageTitled = withTitle({ component: PersonalPage, title: 'Personal' });
+const SolsticePageTitled = withTitle({ component: SolsticePage, title: 'Solstice' });
+const NMFPageTitled = withTitle({ component: NMFPage, title: 'NMF' });
+const GoPageTitled = withTitle({ component: GoPage, title: 'Gemini Offseason' });
+const HunchPageTitled = withTitle({ component: HunchPage, title: 'NPR Hunch' });
+
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/academics" component={AcademicsPage} />
-          <Route exact path="/freelance" component={FreelancePage} />
-          <Route exact path="/personal" component={PersonalPage} />
-          <Route exact path="/solstice" component={SolsticePage} />
-          <Route exact path="/nmf" component={NMFPage} />
-          <Route exact path="/go" component={GoPage} />
-          <Route exact path="/hunch" component={HunchPage} />
+          <Route exact path="/" component={HomePageTitled} />
+          <Route exact path="/academics" component={AcademicsPageTitled} />
+          <Route exact path="/freelance" component={FreelancePageTitled} />
+          <Route exact path="/personal" component={PersonalPageTitled} />
+          <Route exact path="/solstice" component={SolsticePageTitled} />
+          <Route exact path="/nmf" component={NMFPageTitled} />
+          <Route exact path="/go" component={GoPageTitled} />
+          <Route exact path="/hunch" component={HunchPageTitled} />
           {/* <Route exact path="/spotify" component ={SpotifyPage} /> */}
         </Switch>
       </BrowserRouter>
--- src/pages/Hunch.js
+++ src/pages/Hunch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
+// import { render } from "react-dom";
 import "../css/hunch.css"
 import PDF from "../hunch.pdf"
 
